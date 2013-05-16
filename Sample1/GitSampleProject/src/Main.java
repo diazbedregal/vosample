b16473f4cf61b06b6ddbc7956f5ddd9720835a9e
wdiazbe <waldo.diaz-bedregal@valueoptions.com>
2013-05-16 10:58:47 -0400

    Committing from WS2  (Commit & Push)

---
+++ Sample1/GitSampleProject/src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("example");
+		addedByWS2();
+
+	}
+
+
+	public static void addedByWS2() {
+		System.out.println("this method was added by Workspace 2");
+
+	}
+	
+	
+	
+}
