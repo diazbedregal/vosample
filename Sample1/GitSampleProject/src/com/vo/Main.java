192d942470d0e88c5a88bec18b9546603c791d4f
unknown <wdiazbe@DCA-W7D-WDIAZ.VALUEOPTIONS.FHC1.NET>
2013-05-17 15:21:33 -0400

    testing commit

---
+++ Sample1/GitSampleProject/src/com/vo/Main.java
@@ -0,0 +1,26 @@
+package com.vo;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("example");
+		addedByWS2();
+
+	}
+
+
+	public static void addedByWS1() {
+		System.out.println("this method was added by Workspace 1");
+
+	}
+	
+	public static void addedByWS2() {
+		System.out.println("this method was added by Workspace 2");
+
+	}
+	
+}
