a74873971e304ef04a5e6a4c4b12c4b2226c747e
wdiazbe <waldo.diaz-bedregal@valueoptions.com>
2013-05-17 15:08:54 -0400

    commiting file to local repository, and trying to push it to remote

---
+++ Sample1/GitSampleProject/src/AlreadyOnGitHub.java
@@ -0,0 +1,14 @@
+
+public class AlreadyOnGitHub {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("this class was creted after the project was added to github");
+		
+	}
+
+}
