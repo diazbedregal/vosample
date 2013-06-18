14a7da322cf9c91b3cb98e8470dc6a6aa38b02ea
wdiazbe <waldo.diaz-bedregal@valueoptions.com>
2013-06-18 10:10:30 -0400

    Added replace statements for other special characters.

--- Sample1/GitSampleProject/src/ReplaceBlankSpace.java
+++ Sample1/GitSampleProject/src/ReplaceBlankSpace.java
@@ -1,76 +1,95 @@
 import java.io.File;
 
-
 public class ReplaceBlankSpace {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-		
 		String dir = "\\\\vo-dca-devjira\\c$\\Program Files\\Atlassian\\Application Data\\JIRA\\import\\attachments\\P19\\Attachment";
 
-		//File directory = new File(dir); 
-		//File[] files = directory.listFiles();
-		
-		
-		 //File myFile = new File("C:" + File.separator);
-		
-		 File myFile = new File(dir);
-		 
-		 int  count = 0;
+		File myFile = new File(dir);
 
-		 
-		    for(File s: myFile.listFiles()){
+		int count = 0;
+		int notRenamed = 0;
+		int dots = 0;
 
+		for (File s : myFile.listFiles()) {
 
-				 File mySubFolder = new File(dir+"\\" + s.getName());
+			File mySubFolder = new File(dir + "\\" + s.getName());
 
-				// System.out.print(s.getName()+ " ");
-		    	
-				 for(File f: mySubFolder.listFiles()){
-					 
-					// System.out.println(f.getName());
-					 
-					 String originalName = f.getName();
-					// System.out.println(originalName);					 
-					 
-					 String woBlanks = dir+"\\" + s.getName() +"\\" + originalName.replaceAll(" ", "_");
-					 
-					 
-					//System.out.println(originalName);
-					 
-					 
-					 //if (woBlanks.contains("D101914")){
-					 if (!woBlanks.equals(dir+"\\" + s.getName() +"\\" + f.getName())){
-					 
-						 System.out.println("Renaming [" +  ++count +"]"+  woBlanks);
-						 
-						 
-						 f.renameTo(new File(woBlanks));
-					 
-					}else {
+			for (File f : mySubFolder.listFiles()) {
 
-						 System.out.println("NOT Renaming"+  woBlanks);
+				// System.out.println(f.getName());
 
+				String originalName = new String(f.getName() + "");
+				String newName = new String(f.getName() + "");
+
+				newName = newName
 						
+						//Replacing blanks
+						.replaceAll(" ", "_")
+						
+						
+						//Removing apostrophes
+						.replaceAll("’", "")
+						.replaceAll("‘", "")
+						.replaceAll("'", "")
+						
+						//Replacing long dash with short dash
+						.replaceAll("–", "-")
+						
+						//Replacing special chars
+						.replaceAll("#", "_")
+						.replaceAll("\\[", "_")
+						.replaceAll("\\]", "_")
+						.replaceAll("\\%", "_")
+						.replaceAll("û", "_")
+						.replaceAll("\\?", "_");
+				
+
+				String woBlanks = dir
+						+ "\\"
+						+ s.getName()
+						+ "\\"
+						+ newName;
+
+				// – -
+
+				// ’ '
+
+				if (!originalName.equals(newName)) {
+
+					System.out.println("");
+					System.out.println("Renaming [" + ++count + "]");
+					System.out.println("      From     : " + originalName);
+					System.out.println("      To       : " + newName);
+					System.out.println("      Full path: " + woBlanks);
+
+					//f.renameTo(new File(woBlanks));
+
+				} else {
+
+					notRenamed++;
+
+					dots++;
+
+					if (dots > 40) {
+
+						System.out.print(".");
+
+						dots = 0;
+
 					}
-					 
-				 }
-				 
-		    	
-		    	
-		    	
-		    	
-		    }
-		
-		
-		//File file = new File("c:\\text.txt");
-	   // file.renameTo(new File("c:\\text.txt.bak"));
-		
-		
+					// System.out.println("NOT Renaming"+ woBlanks);
+
+				}
+
+			}
+
+		}
+
+		// File file = new File("c:\\text.txt");
+		// file.renameTo(new File("c:\\text.txt.bak"));
+
 	}
 
 }
